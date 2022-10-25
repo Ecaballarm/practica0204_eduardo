1620c98c4c0c062fd7e05a3e1a9316a93b4b5f33
Eduardo <113341087+Ecaballarm@users.noreply.github.com>
2022-10-25 13:01:18 +0200

    Delete Ejercicio 11.py

--- Ejercicio 11.py
+++
@@ -1,7 +0,0 @@
-# Escribir un programa que pida al usuario una palabra y luego
-# muestre por pantalla una a una las letras de la palabra introducida empezando por la última.
-
-palabra = input("Introduce una palabra cualquiera: ")
-
-for i in range(len(palabra)-1, -1, -1):
-    print(palabra[i])
